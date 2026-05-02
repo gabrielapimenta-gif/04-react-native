033cbccba6c97dd5c4fbf65bf590df493baffc29
gabrielapimenta-gif <gabrielapimenta2006@gmail.com>
2026-05-02 13:42:37 -0300

    gabrielapimenta-gif

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Slot } from 'expo-router';
+import { ContadorProvider } from './context/ContadorContext';
+export default function Layout() {
+    return (
+        <ContadorProvider>
+            <Slot />
+        </ContadorProvider>
+    );
+}
